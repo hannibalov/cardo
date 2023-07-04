7efb3f79fd1327ae9d5600d4d3065b0be8a5c219
hannibalov <rodrigo.pizarro.lozano@gmail.com>
2023-07-04 19:04:53 +0200

    added signup functionality through firebase renamed firebase.ts to config.ts added a button in the app to test the signup

--- library/src/App.tsx
+++ library/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {signInWithGoogle} from './firebase/providers';
 
 function App() {
+
+  const handleLoginWithGoogle = async () => {
+    await signInWithGoogle()
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +15,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button className="btn-signup" onClick={handleLoginWithGoogle}>Sign up</button>
         <a
           className="App-link"
           href="https://reactjs.org"
