90439a387a40017477a9444c470809c914041410
hannibalov <hannibalov@users.noreply.github.com>
2023-08-03 09:52:52 +0200

    Firebase database (#7)

---
+++ src/firebase/db.ts
@@ -0,0 +1,37 @@
+import {Book} from '../data/book'
+import {firestore} from './config'
+import {addDoc, collection, getDocs, getDoc, doc, setDoc} from 'firebase/firestore'
+
+export const upsertBook = async (book: Book) => {
+  if (!book.id) {
+    const booksRef = collection(firestore, 'books')
+    delete book.id
+    await addDoc(booksRef, book)
+  } else {
+    const booksRef = doc(firestore, 'books', book.id)
+    await setDoc(booksRef, book)
+  }
+}
+
+export const getBook = async (id: string) => {
+  const booksRef = doc(firestore, 'books', id)
+  return (await getDoc(booksRef)).data()
+}
+
+export const getBooks = async () => {
+  const booksRef = collection(firestore, 'books')
+
+  const snapshot = await getDocs(booksRef)
+
+  const books: Book[] = []
+
+  snapshot.forEach((doc) => {
+    books.push({
+      id: doc.id,
+      title: doc.data().title,
+      author: doc.data().author,
+      imageUrl: doc.data().imageUrl,
+    })
+  })
+  return books
+}
