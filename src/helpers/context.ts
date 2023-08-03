02d45e97c6431e3d40326b80e1a4f10f0aadc62b
hannibalov <rodrigo.pizarro.lozano@gmail.com>
2023-08-03 11:26:19 +0200

    Upserting books with their covers

---
+++ src/helpers/context.ts
@@ -0,0 +1,38 @@
+import {createContext} from 'react'
+
+export type LoginModalContextType = {
+  isLoginModalOpen: boolean
+  setIsLoginModalOpen: (open: boolean) => void
+}
+
+export const LoginModalContext = createContext<LoginModalContextType>({
+  isLoginModalOpen: false,
+  setIsLoginModalOpen: (open: boolean) => {},
+})
+export type ProfileModalContextType = {
+  isProfileModalOpen: boolean
+  setIsProfileModalOpen: (open: boolean) => void
+}
+
+export const ProfileModalContext = createContext<ProfileModalContextType>({
+  isProfileModalOpen: false,
+  setIsProfileModalOpen: (open: boolean) => {},
+})
+
+export enum ActionTypeEnum {
+  ADD,
+  REMOVE,
+  UPDATE,
+  LIST,
+  NONE,
+}
+
+export type ActionType = {
+  action: {type: ActionTypeEnum; bookId?: string}
+  setAction: (action: {type: ActionTypeEnum; bookId?: string}) => void
+}
+
+export const ActionContext = createContext<ActionType>({
+  action: {type: ActionTypeEnum.NONE},
+  setAction: (action: {type: ActionTypeEnum; bookId?: string}) => {},
+})
