041bd093b56574f8ab4767e2f278afed5c242e45
hannibalov <hannibalov@users.noreply.github.com>
2023-08-06 18:13:56 +0200

    Refactored dashboard simplifying it (#14)

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,7 +5,7 @@ import {ActionContext, ActionTypeEnum} from '../helpers/context'
 import {UpsertBook} from './UpsertBook'
 
 export const Dashboard = () => {
-  const [action, setAction] = React.useState({type: ActionTypeEnum.NONE})
+  const [action, setAction] = React.useState({type: ActionTypeEnum.LIST})
 
   useEffect(() => {
     if (action.type === ActionTypeEnum.REFRESH) {
@@ -16,29 +16,17 @@ export const Dashboard = () => {
   const logout = async () => {
     await logoutFirebase()
   }
-  const addBook = async () => {
-    setAction({type: ActionTypeEnum.ADD})
-  }
-  const listBooks = async () => {
-    setAction({type: ActionTypeEnum.LIST})
-  }
   return (
     <ActionContext.Provider value={{action, setAction}}>
-      <button className="btn-logout" onClick={logout}>
-        Sign out
-      </button>
-      {action.type === ActionTypeEnum.NONE && (
-        <>
-          <button className="btn-add" onClick={addBook}>
-            Add book
-          </button>
-          <button className="btn-list" onClick={listBooks}>
-            List books
-          </button>
-        </>
-      )}
-      {(action.type === ActionTypeEnum.ADD || action.type === ActionTypeEnum.UPDATE) && <UpsertBook />}
-      {action.type === ActionTypeEnum.LIST && <BooksGrid />}
+      <div className="top-container">
+        <button className="button" onClick={logout}>
+          Sign out
+        </button>
+      </div>
+      <div className="dashboard-container">
+        {(action.type === ActionTypeEnum.ADD || action.type === ActionTypeEnum.UPDATE) && <UpsertBook />}
+        {action.type === ActionTypeEnum.LIST && <BooksGrid />}
+      </div>
     </ActionContext.Provider>
   )
 }
